ef18bb41755a9052a269f5950fab48c27905c328
RajashekarRaju <rajashekarraju123@gmail.com>
2021-09-17 03:26:12 +0530

    Added new example to create simple list to detail with navigation

---
+++ app/src/main/java/com/pbaileyapps/layoutbasics/ui/listToDetail/Cat.kt
@@ -0,0 +1,9 @@
+package com.pbaileyapps.layoutbasics.ui.listToDetail
+
+data class Cats(
+    val catId: Int,
+    val catImage: Int,
+    val catName: String,
+    val catDescription: String,
+    val catAbout: String,
+)
